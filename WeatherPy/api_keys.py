d4962dbe45f5f727173fe9744d257f8efe4073e4
Krystal Lewis <klew1230@gmail.com>
2022-09-06 21:10:01 -0500

    progress

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e539893c8abdd5c06466020fea88296f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDwZiR6Dd4hXejSgqy3x-SokJAx2ux1GyE"
